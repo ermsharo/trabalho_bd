944c4d0900aa8f712727997a396d03741f103f96
ermsharo <ermsharo@gmail.com>
2024-05-02 21:41:49 -0300

    add relative uncomplete models

--- app/models.py
+++ app/models.py
@@ -5,30 +5,58 @@ from sqlalchemy import Index
 
 db = SQLAlchemy()
 
-# class Card_image(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     NAME = db.Column(db.Text)
-#     DESCRIPTION = db.Column(db.Text)
-#     CLASS = db.Column(db.Text)
-#     EFFECT = db.Column(db.Text)
-#     VISUAL_DESCRIPTION = db.Column(db.Text)
-#     ATK = db.Column(db.Text)
-#     DEF = db.Column(db.Text)
-#     TYPE = db.Column(db.Text)
-#     CATEGORY = db.Column(db.Text)
-#     COST = db.Column(db.Text)
-#     COST_TYPE = db.Column(db.Text)
-#     IMAGE_APROVED = db.Column(db.Text)
-#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+class ESPECIALIDADE(db.Model):
+    indice = db.Column(db.Integer, primary_key=True)
+    nome_especialidade = db.Column(db.Text)
+    id_especialidade = db.Column(db.Integer)
+
+class AGENDA(db.Model):
+    id_agenda = db.Column(db.Integer, primary_key=True)
+    dia_semana = db.Column(db.Text)
+    hora_inicio = db.Column(db.Text)
+    hora_fim = db.Column(db.Text)
+    crm = db.Column(db.Text)
+
+class EXERC_ESP(db.Model):
+    crm = db.Column(db.Integer, primary_key=True)
+    id_especialidade = db.Column(db.Text)
+
+class MEDICO(db.Model):
+    crm = db.Column(db.Integer, primary_key=True)
+    nome_medico = db.Column(db.Text)
+    telefone_medico = db.Column(db.Text)
+    porcentual = db.Column(db.Text)
+
+class DOENCA(db.Model):
+    id_doenca = db.Column(db.Integer, primary_key=True)
+    nome_doenca = db.Column(db.Text)
 
 
-# class GeneratedImage(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     image_prompt = db.Column(db.Text)
-#     image_card = db.Column(db.Text)
-#     image_url = db.Column(db.Text)
-#     valid = db.Column(db.Integer)
-#     status = db.Column(db.Integer)
-#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+class CONSULTA(db.Model):
+    id_consulta = db.Column(db.Integer, primary_key=True)
+    crm = db.Column(db.Text)
+    id_especialidade = db.Column(db.Text)
+    id_paciente = = db.Column(db.Text)
+    data = db.Column(db.Text)
+    hora_inicio =  db.Column(db.Text)
+    hora_fim = db.Column(db.Text)
+    pagou = db.Column(db.Text)
+    valor_pago = db.Column(db.Text)
+    forma_pagamento = db.Column(db.Text)
+
+class DIAGNOSTICO(db.Model):
+    id_diagnostico = db.Column(db.Integer, primary_key=True)
+    tratamento_recomendado = db.Column(db.Text)
+    remedios_receitados = db.Column(db.Text)
+    observacoes = = db.Column(db.Text)
+    id_consulta = db.Column(db.Text)
+
+
+class PACIENTE(db.Model):
+    id_paciente = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.Text)
+    endereco = db.Column(db.Text)
+    nome_paciente  = db.Column(db.Text)
+    telefone_paciente = db.Column(db.Text)
+    sexo = db.Column(db.Text)
+    idade = db.Column(db.Text)
