5a604e71310cbfdb72f5d7c6859f6856bcd8046b
Emilio Haro <ermsharo@gmail.com>
2024-05-05 13:37:11 -0300

    update or changes

--- app/app.py
+++ app/app.py
@@ -4,22 +4,18 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_migrate import Migrate
-from APP.models import db
+from models import db
 
-# from routes.logs import logs_route
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from datetime import datetime
-# from run_updates import update_system_rt
 
 load_dotenv()
 
 app = Flask(__name__)
 
 DB_STRING = os.getenv("DB_STRING")
-# Update the SQLALCHEMY_DATABASE_URI with your PostgreSQL connection URL
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_STRING
 db.init_app(app)
 migrate = Migrate(app, db, render_as_batch=True)
+
 CORS(
     app,
     origins="*",
@@ -27,13 +23,6 @@ CORS(
     supports_credentials=True,
     allow_headers=["Content-Type", "Authorization", "test"],
 )
-
-
-@app.before_request
-def authenticate_request():
-    query_parameters = request.args
-    print("query_parameters", query_parameters, flush=True)
-    print("token", query_parameters.get("rt_token"), flush=True)
 
 
 @app.route("/")
--- app/models.py
+++ app/models.py
@@ -6,11 +6,13 @@ from sqlalchemy import Index
 db = SQLAlchemy()
 
 class ESPECIALIDADE(db.Model):
+    __tablename__ = "especialidade"
     indice = db.Column(db.Integer, primary_key=True)
     nome_especialidade = db.Column(db.Text)
     id_especialidade = db.Column(db.Integer)
 
 class AGENDA(db.Model):
+    __tablename__ = "agenda"
     id_agenda = db.Column(db.Integer, primary_key=True)
     dia_semana = db.Column(db.Text)
     hora_inicio = db.Column(db.Text)
@@ -18,33 +20,38 @@ class AGENDA(db.Model):
     crm = db.Column(db.Text)
 
 class EXERC_ESP(db.Model):
+    __tablename__ = "exerc_esp"
     crm = db.Column(db.Integer, primary_key=True)
     id_especialidade = db.Column(db.Text)
 
 class MEDICO(db.Model):
+    __tablename__ = "medico"
     crm = db.Column(db.Text, primary_key=True)
     nome_medico = db.Column(db.Text)
     telefone_medico = db.Column(db.Text)
-    porcentual = db.Column(db.float)
+    porcentual = db.Column(db.Float)
 
 class DOENCA(db.Model):
+    __tablename__ = "doenca"
     id_doenca = db.Column(db.Integer, primary_key=True)
     nome_doenca = db.Column(db.Text)
 
 
 class CONSULTA(db.Model):
+    __tablename__ = "consulta"
     id_consulta = db.Column(db.Integer, primary_key=True)
     crm = db.Column(db.Text)
     id_especialidade = db.Column(db.Text)
     id_paciente =  db.Column(db.Text)
     data = db.Column(db.Date)
-    hora_inicio =  db.Column(db.Datetime)
-    hora_fim = db.Column(db.Datetime)
+    hora_inicio =  db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    hora_fim = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     pagou = db.Column(db.Text)
     valor_pago = db.Column(db.Text)
     forma_pagamento = db.Column(db.Text)
 
 class DIAGNOSTICO(db.Model):
+    __tablename__ = "diagnostico"
     id_diagnostico = db.Column(db.Integer, primary_key=True)
     tratamento_recomendado = db.Column(db.Text)
     remedios_receitados = db.Column(db.Text)
@@ -53,10 +60,11 @@ class DIAGNOSTICO(db.Model):
 
 
 class PACIENTE(db.Model):
+    __tablename__ = "paciente"
     id_paciente = db.Column(db.Integer, primary_key=True)
     cpf = db.Column(db.Text)
     endereco = db.Column(db.Text)
     nome_paciente  = db.Column(db.Text)
     telefone_paciente = db.Column(db.Text)
     sexo = db.Column(db.Text)
-    idade = db.Column(db.Text)
+    idade = db.Column(db.Integer)
